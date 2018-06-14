5b88935db64157e39ce0a7d712537d2c7e5a52a2
ZhidkovGV <zhidkov.gleb@gmail.com>
2018-06-14 17:44:28 +0300

    added styles dir

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-import {Observable ,fromEvent} from 'rxjs'
+import { fromEvent } from 'rxjs';
 
+const el = document.body;
+
+// Create an Observable that will publish mouse movements
+const mouseMoves = fromEvent(el, 'click');
+
+// Subscribe to start listening for mouse-move events
+const subscription = mouseMoves.subscribe((event) => {
+    // Log coords of mouse movements
+    console.log(`Coords: ${event.clientX} X ${event.clientY}`);
+
+    // When the mouse is over the upper-left of the screen,
+    // unsubscribe to stop listening for mouse movements
+    if (event.clientX < 40 && event.clientY < 40) {
+        subscription.unsubscribe();
+    }
+});
