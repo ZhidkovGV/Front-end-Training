e89c9c61b117646286a52f601d3946e1b4df775b
ZhidkovGV <zhidkov.gleb@gmail.com>
2018-06-15 17:44:53 +0300

    feature: hidable autocompete

--- dist/main.js
+++ dist/main.js
@@ -12671,26 +12671,32 @@ const input = document.querySelector('input');
 const autoComplete = document.querySelector('.auto-complete');
 const autoCompleteItems = Array.from(document.querySelectorAll('.auto-complete-items li'));
 const inputChanges = Object(rxjs__WEBPACK_IMPORTED_MODULE_0__["fromEvent"])(input,'input');
+const complete = Object(rxjs__WEBPACK_IMPORTED_MODULE_0__["fromEvent"])(autoComplete, 'click');
 
 const render = (info) => {
+    autoComplete.style.display = "block";
     autoCompleteItems.forEach((item, index) => {
         item.innerText = info[index].name || ""
     });
 };
 
 const requestInfo = (req) => {
-    fetch(`https://api.github.com/search/repositories?q=${req}+in:name&client_id=41af896cd9f20012e512&client_secret=ae08199ed8be428e92e742a8eca4d8d4aeff4e9b`)
-        .catch(err => console.log(err))
+    return fetch(`https://api.github.com/search/repositories?q=${req}+in:name&client_id=41af896cd9f20012e512&client_secret=ae08199ed8be428e92e742a8eca4d8d4aeff4e9b`)
         .then(res => res.json())
-        .then(res => render(res.items))
 };
 
-inputChanges.subscribe((event) => {
-    const response = requestInfo(event.target.value);
-    /*render(response.items)*/
-
+inputChanges.subscribe(async(event) => {
+    const response = await requestInfo(event.target.value);
+    render(response.items)
 });
 
+complete.subscribe((event) => {
+    console.log(event);
+    if(event.target.tagName === "LI") {
+        input.value = event.target.innerText;
+        autoComplete.style.display = "none"
+    }
+});
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -5,21 +5,29 @@ const input = document.querySelector('input');
 const autoComplete = document.querySelector('.auto-complete');
 const autoCompleteItems = Array.from(document.querySelectorAll('.auto-complete-items li'));
 const inputChanges = fromEvent(input,'input');
+const complete = fromEvent(autoComplete, 'click');
 
 const render = (info) => {
+    autoComplete.style.display = "block";
     autoCompleteItems.forEach((item, index) => {
         item.innerText = info[index].name || ""
     });
 };
 
 const requestInfo = (req) => {
-    fetch(`https://api.github.com/search/repositories?q=${req}+in:name&client_id=41af896cd9f20012e512&client_secret=ae08199ed8be428e92e742a8eca4d8d4aeff4e9b`)
+    return fetch(`https://api.github.com/search/repositories?q=${req}+in:name&client_id=41af896cd9f20012e512&client_secret=ae08199ed8be428e92e742a8eca4d8d4aeff4e9b`)
         .then(res => res.json())
-        .then(res => render(res.items))
 };
 
-inputChanges.subscribe((event) => {
-    const response = requestInfo(event.target.value);
-    /*render(response.items)*/
+inputChanges.subscribe(async(event) => {
+    const response = await requestInfo(event.target.value);
+    render(response.items)
+});
 
+complete.subscribe((event) => {
+    console.log(event);
+    if(event.target.tagName === "LI") {
+        input.value = event.target.innerText;
+        autoComplete.style.display = "none"
+    }
 });
